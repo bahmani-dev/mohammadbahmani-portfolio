ac3ead2d1f9067a88f26eaf40ac93df37561af89
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-13 11:38:46 +0430

    center the elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <main className="f">
+    <main className="flex flex-col items-center ">
       <Header></Header>
       <Hero></Hero>
     </main>
