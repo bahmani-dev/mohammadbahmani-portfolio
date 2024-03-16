acc6bb9d880be49d9912bed819bf74f89d174e53
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-16 22:01:57 +0430

    add height

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className=" container text-white py-4 px-6 md:px-10 lg:px-16 flex items-center justify-between">
+    <header className=" container text-white py-4 px-6 md:px-10 lg:px-16 flex items-center justify-between h-20">
       {/* Left Part */}
       <div className="flex items-center space-x-4">
         {/* Icon */}
@@ -25,37 +25,34 @@ const Header = () => {
       </div>
 
       {/* Middle Part */}
-      <nav className="flex-grow text-center">
-        <ul className="flex justify-center space-x-6 text-black">
-          <li>
-            <a href="#" className=" hover:text-yellow-500 text-black">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#" className=" hover:text-yellow-500 text-black">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className=" hover:text-yellow-500 text-black">
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="#" className=" hover:text-yellow-500 text-black">
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
 
       {/* Right Part */}
       <div className="flex items-center space-x-4">
         {/* Resume Button */}
-        <button className="bg-yellow-500 text-gray-900 py-2 px-4 rounded-lg hover:bg-yellow-600">
-          Resume
-        </button>
+        <nav className="flex-grow text-center">
+          <ul className="flex justify-center space-x-6 text-black">
+            <li>
+              <a href="#" className=" hover:text-yellow-500 text-black">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className=" hover:text-yellow-500 text-black">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className=" hover:text-yellow-500 text-black">
+                Services
+              </a>
+            </li>
+            <button className="">
+              <a href="#" className=" hover:text-yellow-500 text-black">
+                Contact
+              </a>
+            </button>
+          </ul>
+        </nav>
       </div>
     </header>
   );
