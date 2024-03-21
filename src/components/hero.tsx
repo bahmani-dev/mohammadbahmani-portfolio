0cce5156a17416baa9286e4a9121272d29be2b2a
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-21 14:15:19 +0430

    add image and reform the texts

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,13 +3,15 @@ import Header from "@/components/header";
 
 const HeroSection = () => {
   return (
-    <div className="hero relative overflow-hidden container h-[700px]">
+    <div className="hero relative overflow-hidden container h-[700px] ">
       <Header></Header>
-      <div className="grid grid-cols-12 relative bottom-20 z-10 ">
-        {/* <div className="absolute inset-0 bg-white"></div>
-        <div className="absolute inset-0 bg-black "></div> */}
-        <div className="w-full h-full bg-white col-span-5"></div>
-        <img src="/images/blackBg.svg" alt="" className="col-span-7" />
+      <div className="grid grid-cols-12 relative bottom-20 z-10 h-full ">
+        <div className="w-full h-full col-span-5"></div>
+        <img
+          src="/images/blackBg.svg"
+          alt=""
+          className="col-span-7 h-full lg:w-[160%] "
+        />
       </div>
       <div className="grid grid-cols-12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  z-10 text-white w-full">
         <div className=" col-span-5 ">
@@ -26,7 +28,7 @@ const HeroSection = () => {
             </div>
           </div>
         </div>
-        <div className="text-lg md:text-xl lg:text-2xl col-span-7">
+        <div className="text-lg md:text-xl lg:text-2xl col-span-6 col-start-7">
           This is a diagonally divided hero page.
         </div>
       </div>
