5bfda7cde59a9fdd135aec0beb921ceeb9f52d0d
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-21 14:14:59 +0430

    add a black background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import Header from "@/components/header";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center ">
+    <main className=" flex flex-col items-center ">
       {/* <Header></Header> */}
+
+      <div className="self-end absolute h-[700px] w-[45%] bg-black"></div>
       <Hero></Hero>
     </main>
   );
