f1bcc933bdbda2836ca8d78813ed17482d0cd00b
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-30 21:55:23 +0430

    add skills section

--- src/components/summary.tsx
+++ src/components/summary.tsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const Box = () => {
   return (
-    <div className=" p-8 shadow-lg  mx-auto flex   justify-center summary relative bottom-[50px] z-30 rounded-[30px]">
-      <h2 className="text-xl text-white font-bold mb-3">Summary</h2>
-      <p className="text-white mb-4 text-sm font-thin ">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis ut
-      </p>
-      <button className="uppercase border-l text-sm text-white border-r border-transparent border-gray-200 px-4 py-1 hover:bg-gray-200 focus:outline-none">
-        Read More
-      </button>
+    <div className="bg-black h-[200px] flex items-center justify-center w-full">
+      <div className="container mx-auto flex justify-evenly">
+        {/* Add your sponsor logos here */}
+        <div className="mx-4 h-20  w-20 bg-gray-400"></div>
+        <div className="mx-4 h-20  w-20 bg-gray-400"></div>
+        <div className="mx-4 h-20  w-20 bg-gray-400"></div>
+        <div className="mx-4 h-20  w-20 bg-gray-400"></div>
+        {/* Add more sponsors as needed */}
+      </div>
     </div>
   );
 };
