d97b896fe119fa7ab70c08e55c7c4a44334f1edd
mandegar20 <mohammadmandegar2019@gmail.com>
2024-03-31 12:32:22 +0430

    delete img and roate the div

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,18 +3,14 @@ import Header from "@/components/header";
 
 const HeroSection = () => {
   return (
-    <div className="hero">
-      <div className=" relative overflow-hidden container h-[700px] ">
+    <div className="hero self-stretch flex justify-center">
+      <div className=" relative overflow-hidden container lg:max-xl:px-10 h-[700px] ">
         <Header></Header>
         <div className="grid grid-cols-12 relative bottom-20 z-10 h-full ">
-          <div className="w-full h-full col-span-5"></div>
-          <img
-            src="/images/blackBg.svg"
-            alt=""
-            className="col-span-7 h-full lg:w-[160%] "
-          />
+          <div className="w-full h-full xl:col-span-7 col-span-6 "></div>
+          <div className="xl:col-span-5 col-span-6 bg-black rotate-3 scale-125"></div>
         </div>
-        <div className="grid grid-cols-12 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  z-10 text-white w-full">
+        <div className="grid grid-cols-12 absolute lg:max-xl:px-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  z-10 text-white w-full">
           <div className=" col-span-5 ">
             <h1 className="text-sm md:text-sm lg:text-xl font-bold mb-4  text-black">
               Hi, I am
