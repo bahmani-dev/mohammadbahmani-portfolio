e54ef40eca8bc80072c2da347799dcd52dde04d9
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-07 10:11:57 +0430

    add card

--- src/app/my-portfolio/page.tsx
+++ src/app/my-portfolio/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/header";
+import ProjectCard from "@/components/projectCard";
 import React from "react";
 
 const PortfolioPage = () => {
@@ -7,8 +8,9 @@ const PortfolioPage = () => {
       id: 1,
       name: "Project 1",
       imageUrl:
-        "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
       liveDemoUrl: "https://example.com",
+      color: "#f58585",
     },
     {
       id: 2,
@@ -16,18 +18,20 @@ const PortfolioPage = () => {
       imageUrl:
         "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
       liveDemoUrl: "https://example.com",
+      color: "#00ff00",
     },
     {
       id: 3,
       name: "Project 3",
       imageUrl:
-        "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+        "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
       liveDemoUrl: "https://example.com",
+      color: "#0000ff",
     },
     // Add more projects as needed
   ];
   return (
-    <div className=" flex flex-col items-center bg-[#0f0f0f] gap-10 h-screen">
+    <div className=" flex flex-col items-center bg-[#0f0f0f] gap-10 ">
       <h1 className="uppercase text-[52px] font-bold text-white mt-10 relative z-10">
         <span className="text-yellow-500">my</span> portfolio
       </h1>
@@ -37,25 +41,9 @@ const PortfolioPage = () => {
       <div className="self-end absolute h-[100%] pr-5">
         <Header></Header>
       </div>
-      <div className="container grid grid-cols-1 md:grid-cols-2 gap-4 max-w-[80%]">
-        {projects.map((project) => (
-          <div key={project.id} className="relative ">
-            <img
-              src={project.imageUrl}
-              alt={project.name}
-              className="w-full h-auto rounded-lg "
-            />
-            <a
-              href={project.liveDemoUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="absolute inset-0 flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50 rounded-lg"
-            >
-              <button className="bg-yellow-500 hover:bg-yellow-700 transition-all ease-in-out duration-300 text-white font-bold py-2 px-4 rounded">
-                Live Demo
-              </button>
-            </a>
-          </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-[80%]">
+        {projects.map((project, index) => (
+          <ProjectCard key={index} project={project} />
         ))}
       </div>
     </div>
