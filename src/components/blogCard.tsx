86e2e9f6ce9cb7c73fadbeec8a52e7d08e42d39b
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-07 13:01:11 +0430

    add blogscard ☺️

---
+++ src/components/blogCard.tsx
@@ -0,0 +1,32 @@
+// components/BlogCard.tsx
+import React from "react";
+import Link from "next/link";
+
+interface Blog {
+  title: string;
+  image: string;
+  text: string;
+  slug: string;
+}
+
+interface BlogCardProps {
+  blog: Blog;
+}
+
+const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
+  return (
+    <Link href={`/blogs/${blog.slug}`}>
+      <div className="rounded-md border  overflow-hidden text-white ">
+        <img src={blog.image} alt={blog.title} className="w-full h-1/2" />
+        <div className="bg-yellow-500 h-1 w-full "></div>
+        <div className="p-4  h-1/2">
+          <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
+          <p>{blog.text}</p>
+        </div>
+      </div>
+    </Link>
+  );
+  4;
+};
+
+export default BlogCard;
