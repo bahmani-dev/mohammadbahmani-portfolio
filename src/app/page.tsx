8754bd9e4896fd2268a24d3ff128598be67b0ed0
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-08 10:10:50 +0430

    add page wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-"use-client";
+"use client";
 import Image from "next/image";
 import Hero from "@/components/hero";
 import Box from "@/components/summary";
 import Header from "@/components/header";
 import React from "react";
+import { PageWrapper } from "./pageWrapper";
 export default function Home() {
   return (
-    <main className=" flex flex-col items-center ">
-      <div className="lg:self-end absolute lg:h-[100%]  w-full flex justify-center lg:pr-5">
-        <Header></Header>
-      </div>
+    <PageWrapper>
+      <main className=" flex flex-col items-center ">
+        <div className="lg:self-end absolute lg:h-[100%]  w-full flex justify-center lg:pr-5">
+          <Header></Header>
+        </div>
 
-      <div className="self-end absolute h-screen w-[40%] bg-[#111] z-0 "></div>
-      {/* <div className="self-start absolute h-[700px] w-[20%] hero "></div> */}
+        <div className="self-end absolute h-screen w-[40%] bg-[#111] z-0 "></div>
+        {/* <div className="self-start absolute h-[700px] w-[20%] hero "></div> */}
 
-      <Hero></Hero>
-    </main>
+        <Hero></Hero>
+      </main>
+    </PageWrapper>
   );
 }
