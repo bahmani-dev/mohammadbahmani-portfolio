59a3afd33a45d73d254cc21ffcf63e4872df2b5c
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-09 12:03:51 +0430

    add robot mono font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 "use client";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/Transition";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Roboto_Mono({ subsets: ["latin"] });
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
