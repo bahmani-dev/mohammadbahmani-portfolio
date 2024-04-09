586efbd5e90d4b1ea9c9de170bf440171a73c606
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-09 12:05:36 +0430

    add type

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,11 +2,15 @@ import React, { ReactNode, ButtonHTMLAttributes } from "react";
 import { AiOutlineArrowRight } from "react-icons/ai";
 interface Props {
   text: string;
+  type: "submit" | "reset" | "button";
 }
 
-const Button = ({ text }: Props) => {
+const Button = ({ text, type }: Props) => {
   return (
-    <button className="relative inline-flex items-center justify-between  pl-5 py-6   transition duration-300 ease-out border-2  rounded-full shadow-md group  overflow-hidden font-medium        group  h-10 w-60    border-yellow-500     uppercase text-base">
+    <button
+      type={type}
+      className="relative inline-flex items-center justify-between  pl-5 py-6   transition duration-300 ease-out border-2  rounded-full shadow-md group  overflow-hidden font-medium        group  h-10 w-60    border-yellow-500     uppercase text-base"
+    >
       <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 translate-x-full bg-yellow-500 group-hover:-translate-x-0 ease"></span>
       <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease text-white max-md:text-sm">
         {text}
