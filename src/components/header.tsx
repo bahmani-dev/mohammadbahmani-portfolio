b0882b165e698b4c4f7844bde5250572afd46b93
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-15 22:43:59 +0430

    change the logo to light and dark mode changer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import Nav from "./nav";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 const Header = () => {
   return (
     <header className=" max-lg:mt-20 fixed max-lg:w-full z-30 max-lg:bg-[#252525]  text-white py-4 lg:right-5 max-lg:bottom-0  px-0 flex flex-col lg:items-end items-center justify-between lg:h-[70%]">
       {/* Left Part */}
-      <div className="lg:flex items-center space-x-4 hidden">
-        {/* Icon */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-yellow-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M5 13l4 4L19 7"
-          />
-        </svg>
-        {/* Text */}
-        <span className="hidden lg:inline-block relative z-40">Logo</span>
+      <div className="w-[40px] h-[40px] bg-gray-800 rounded-full relative right-5 flex items-center justify-center cursor-pointer ">
+        <BsFillMoonStarsFill />
       </div>
 
       {/* Middle Part */}
