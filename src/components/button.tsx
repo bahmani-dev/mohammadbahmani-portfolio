2912175bcd7e84e7d0708f64d26e8d8df9b40be2
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-16 21:54:56 +0430

    make button font bold

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -12,7 +12,7 @@ const Button = ({ text, type }: Props) => {
       className="relative inline-flex items-center justify-between  pl-5 py-6    transition duration-300 ease-out border-2  rounded-full shadow-md group  overflow-hidden font-medium        group  h-10       border-yellow-500     uppercase text-base"
     >
       <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 translate-x-full bg-yellow-500 group-hover:-translate-x-0 ease"></span>
-      <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease dark:text-white max-md:text-sm pr-5">
+      <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease dark:text-white max-md:text-sm font-bold pr-5">
         {text}
       </span>
 
