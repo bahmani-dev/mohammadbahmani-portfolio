af1da88d4f7d16c737d47509d5bd86df5ffceb4c
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-16 18:30:04 +0430

    use nextjs Image

--- src/components/blogCard.tsx
+++ src/components/blogCard.tsx
@@ -1,6 +1,7 @@
 // components/BlogCard.tsx
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 interface Blog {
   title: string;
@@ -17,7 +18,13 @@ const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   return (
     <Link href={`${blog.slug}`} target="_blank">
       <div className="rounded-md border  overflow-hidden text-[#666] dark:text-white ">
-        <img src={blog.image} alt={blog.title} className="w-full h-1/2" />
+        <Image
+          width={20}
+          height={20}
+          src={blog.image}
+          alt={blog.title}
+          className="w-full h-1/2"
+        />
         <div className="bg-yellow-500  h-1 w-full "></div>
         <div className="p-4  h-1/2">
           <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
