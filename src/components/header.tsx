c5fccb47849a36410e1772d385deae86859aff3c
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-16 10:35:04 +0430

    add dark mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className=" max-lg:mt-20 fixed max-lg:w-full z-30 max-lg:bg-[#252525]  text-white py-4 lg:right-5 max-lg:bottom-0  px-0 flex flex-col lg:items-end items-center justify-between lg:h-[70%]">
       {/* Left Part */}
-      <div className="w-[40px] h-[40px] bg-gray-800 rounded-full relative right-5 flex items-center justify-center cursor-pointer ">
+      <div className="w-[40px] h-[40px] dark:bg-gray-800 bg-gray-400 rounded-full relative right-5 flex items-center justify-center cursor-pointer ">
         <BsFillMoonStarsFill />
       </div>
 
