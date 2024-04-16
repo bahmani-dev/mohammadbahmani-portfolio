198ed044b0ce70a2c67aaeec179fa8e1a97e517a
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-16 15:45:43 +0430

    setup dark mode

--- src/components/blogCard.tsx
+++ src/components/blogCard.tsx
@@ -16,9 +16,9 @@ interface BlogCardProps {
 const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   return (
     <Link href={`${blog.slug}`} target="_blank">
-      <div className="rounded-md border  overflow-hidden text-white ">
+      <div className="rounded-md border  overflow-hidden text-[#666] dark:text-white ">
         <img src={blog.image} alt={blog.title} className="w-full h-1/2" />
-        <div className="bg-yellow-500 h-1 w-full "></div>
+        <div className="bg-yellow-500  h-1 w-full "></div>
         <div className="p-4  h-1/2">
           <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
           <p>{blog.text}</p>
