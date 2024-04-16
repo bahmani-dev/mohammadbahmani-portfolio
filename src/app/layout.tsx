65fcc4e0220f7cc585edeef317d0bbbdd09e8795
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-16 15:44:05 +0430

    dark mode logic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { PT_Serif } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/Transition";
 import PreloadingScreen from "@/components/PreloadingScreen";
+import React, { useState, useEffect } from "react";
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
 
 const inter = PT_Serif({
   weight: ["400", "700"],
@@ -22,11 +24,31 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("darkMode") === "true";
+    setDarkMode(isDarkMode);
+  }, []);
+
+  const toggleDarkMode = () => {
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    localStorage.setItem("darkMode", newMode.toString());
+  };
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body
+        className={`${inter.className} ${darkMode ? "dark" : "light"} relative`}
+      >
         {" "}
         <PreloadingScreen />
+        <div
+          onClick={toggleDarkMode}
+          className=" fixed z-40 transition duration-300 hover:bg-yellow-500 ease-in-out right-10 top-10  w-[40px] h-[40px] text-white dark:bg-gray-800 bg-gray-400 rounded-full   flex items-center justify-center cursor-pointer "
+        >
+          {darkMode ? <BsFillSunFill /> : <BsFillMoonStarsFill />}
+        </div>
         <TransitionProvider>{children}</TransitionProvider>
       </body>
     </html>
