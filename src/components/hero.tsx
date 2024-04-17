7c5ff2e412b9278a2dcb901b337c6fd7b8b2a866
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-17 22:47:50 +0430

    add image background

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Button from "./button";
 import Link from "next/link";
+import Image from "next/image";
+import { url } from "inspector";
 
 const HeroSection = () => {
   return (
@@ -11,7 +13,20 @@ const HeroSection = () => {
           <div className="col-span-9  bg-[#fff] dark:bg-[#111] -rotate-6 scale-125 lg:shadow"></div>
         </div>
         <div className="lg:h-full grid grid-cols-12 justify-items-center items-center  gap-5 lg:gap-0 absolute lg:max-xl:px-10 top-1/2 left-1/2 transform -translate-x-1/2 sm:-translate-y-1/2 translate-y-[-55%]  z-10 text-white w-full">
-          <div className=" col-span-4 h-[90%] w-[100%] bg-white rounded-[30px] shadow ml-4 hidden lg:block"></div>
+          <div
+            className=" col-span-4 h-[90%] w-[100%] bg-white rounded-[30px] shadow ml-4 hidden lg:block "
+            style={{
+              backgroundImage: "url('/images/hero.jpg')",
+              backgroundPosition: "center",
+            }}
+          >
+            {/* <Image
+              src="/images/hero.jpg"
+              alt="main photo"
+              width={500}
+              height={1200}
+            ></Image> */}
+          </div>
           <div className="w-[250px] h-[250px] rounded-full border-4 border-[#252525] col-span-12 lg:hidden"></div>
           <div className="text-lg md:text-xl lg:text-2xl lg:col-span-8 col-span-12 flex flex-col justify-center  gap-5 sm:w-[70%] w-[100%] p-4 text-[#555] dark:text-white">
             <div className="sm:text-4xl text-2xl font-bold leading-tight max-lg:text-center   ">
@@ -33,10 +48,10 @@ const HeroSection = () => {
               </div>
             </div>
             <p className="xl:leading-[37px] leading-normal lg:leading-loose sm:text-[16px] text-sm font-thin max-lg:text-center xl:w-[550px] mt-3  xl:mt-5 ">
-              I&apos;m a Tunisian based web designer & front‑end developer
-              focused on crafting clean & user‑friendly experiences, I am
-              passionate about building excellent software that improves the
-              lives of those around me.
+              Software Engineer committed to crafting captivating user
+              experiences. Passionate about transforming ideas into impactful
+              digital solutions. Let&apos;s create something remarkable
+              together!
             </p>
             <div className="max-lg:text-center">
               <Link href="/about">
