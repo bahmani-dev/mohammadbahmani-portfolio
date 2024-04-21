22fa11c36e00d5ecaca7d66266b60be63d9857cc
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-21 23:14:35 +0430

    add link

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 interface Project {
   name: string;
   imageUrl: string;
@@ -17,21 +18,22 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   const [isHovered, setIsHovered] = React.useState(false);
   return (
     <div className="">
-      <div
-        className={`flex flex-col items-center justify-center  py-10 px-5 rounded-md bg-[${project.color}]  dark:bg-gray-600 cursor-pointer relative overflow-hidden  `}
+      <Link
+        href={project.liveDemoUrl}
+        className={`flex flex-col items-center justify-center h-[400px]  py-10 px-5 rounded-md bg-[${project.color}]  dark:bg-gray-600 cursor-pointer relative overflow-hidden  `}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
         <Image
           src={project.imageUrl}
           alt={project.name}
-          width={400}
+          width={500}
           height={100}
           className={`max-w-[90%] mb-4 rounded-md transform transition-transform duration-500  ${
             isHovered ? "scale-110" : ""
           }`}
         />
-      </div>
+      </Link>
       <p
         className={` mt-5  dark:text-white text-[#666] text-2xl transform transition duration-500 ${
           isHovered ? "underline underline-offset-8" : ""
@@ -47,4 +49,3 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
 };
 
 export default ProjectCard;
-// style={{ backgroundColor: project.color }}
