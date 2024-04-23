bade5af4948a1233299251e900af434c1476ec1b
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-23 23:13:27 +0430

    change the design the blog cards

--- src/components/blogCard.tsx
+++ src/components/blogCard.tsx
@@ -17,16 +17,16 @@ interface BlogCardProps {
 const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   return (
     <Link href={`${blog.slug}`} target="_blank">
-      <div className="rounded-md border  overflow-hidden text-[#666] dark:text-white ">
+      <div className="rounded-md border  overflow-hidden text-[#666] dark:text-white  ">
         <Image
-          width={20}
-          height={20}
+          width={400}
+          height={300}
           src={blog.image}
           alt={blog.title}
-          className="w-full h-1/2"
+          className="w-full h-[380px]"
         />
         <div className="bg-yellow-500  h-1 w-full "></div>
-        <div className="p-4  h-1/2">
+        <div className="p-4  ">
           <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
           <p>{blog.text}</p>
         </div>
