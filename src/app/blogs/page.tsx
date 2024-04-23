ecace77b09d5072ca5b1b1574bf1ad61eb434675
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-23 23:13:05 +0430

    add new blog

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -6,6 +6,13 @@ import { motion, useScroll, useTransform } from "framer-motion";
 const BlogPage = () => {
   const blogs = [
     {
+      title:
+        "Mastering Object-Oriented Programming in TypeScript: Your Complete Guide with Practical Examples",
+      image: "/images/blog2.png",
+      text: "Embark on an enjoyable learning experience about Object-Oriented Programming in TypeScript. We’ll break down complex concepts with real-world examples and simplified definitions, paving the way for your coding success.",
+      slug: "https://medium.com/@mohammadmandegar2019/mastering-object-oriented-programming-in-typescript-your-complete-guide-with-practical-examples-5f2ad17b1668", // This could be dynamically generated from the title
+    },
+    {
       title: "SOLID Principles: A Must-Know for All Developers",
       image:
         "https://miro.medium.com/v2/resize:fit:720/format:webp/1*NB9wHllrua8T3sBXGDwM1g.jpeg",
