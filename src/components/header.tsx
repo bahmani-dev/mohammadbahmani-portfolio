9966bf5f0c3860c65f22142b7e9f29b83bc4663c
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-28 23:00:06 +0430

    enhance

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,9 @@ import Nav from "./nav";
 const Header = () => {
   return (
     <header
-      className=" max-lg:mt-20 fixed lg:top-1/2 lg:transform lg:-translate-y-[20%] max-lg:w-full z-30 dark:max-lg:bg-[#252525] max-lg:bg-[#666]  text-white py-4 
-    lg:right-5 max-lg:bottom-0 
+      className=" max-lg:mt-20 fixed lg:top-1/2 lg:transform lg:-translate-y-[20%]
+       max-lg:w-full z-20 dark:max-lg:bg-[#252525] max-lg:bg-[#666]  text-white py-4 
+    lg:right-5  bottom-0
      px-0 flex flex-col  items-center justify-between lg:h-[100%]"
     >
       {/* Left Part */}
