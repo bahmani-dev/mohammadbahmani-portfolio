f096cfaec5b357e16a910b50044d2c2a83ed80a5
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-28 22:59:39 +0430

    add image as background in order to preserve its quality in different screen sizes

--- src/components/blogCard.tsx
+++ src/components/blogCard.tsx
@@ -18,13 +18,15 @@ const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   return (
     <Link href={`${blog.slug}`} target="_blank">
       <div className="rounded-md border  overflow-hidden text-[#666] dark:text-white  ">
-        <Image
-          width={400}
-          height={300}
-          src={blog.image}
-          alt={blog.title}
-          className="w-full h-[380px]"
-        />
+        <div
+          className="h-[300px]"
+          style={{
+            backgroundImage: `url(${blog.image})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+          }}
+        ></div>
+
         <div className="bg-yellow-500  h-1 w-full "></div>
         <div className="p-4  ">
           <h2 className="text-xl font-bold mb-2">{blog.title}</h2>
@@ -37,3 +39,12 @@ const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
 };
 
 export default BlogCard;
+{
+  /* <Image
+width={400}
+height={300}
+src={blog.image}
+alt={blog.title}
+className="w-full h-[380px]"
+/> */
+}
