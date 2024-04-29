0814a514732e9dd060ea6d913b1ecaf116224945
mandegar20 <mohammadmandegar2019@gmail.com>
2024-04-29 09:16:44 +0430

    make it more responsive

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -11,10 +11,10 @@ const Button = ({ text, type, icon }: Props) => {
   return (
     <button
       type={type}
-      className="relative inline-flex items-center justify-between  pl-5 py-6    transition duration-300 ease-out border-2  rounded-full shadow-md group  overflow-hidden font-medium        group  h-10       border-yellow-500     uppercase text-base"
+      className="relative inline-flex items-center justify-between  pl-5 py-3 sm:py-6   transition duration-300 ease-out border-2  rounded-full shadow-md group  overflow-hidden font-medium        group  h-10       border-yellow-500     uppercase text-base"
     >
       <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 translate-x-full bg-yellow-500 group-hover:-translate-x-0 ease"></span>
-      <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease dark:text-white max-md:text-sm  pr-5">
+      <span className="relative transition-colors duration-300 delay-200 group-hover:text-white ease dark:text-white sm:text-sm text-[12px]  pr-5">
         {text}
       </span>
 
