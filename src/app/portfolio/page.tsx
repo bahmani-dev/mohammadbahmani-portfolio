c8c6c84a01a5240f2d950de5164266e7792f19ad
mandegar20 <mohammadmandegar2019@gmail.com>
2024-05-13 08:25:00 +0430

    add jobs.af and weena.af

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -12,6 +12,7 @@ const PortfolioPage = () => {
       imageUrl: "/images/portfolio1.png",
       liveDemoUrl: "/artGallery",
       color: "#f58585",
+      target: "_self",
       desc: "        The Colorful Effects Artgallery website offers a vibrant and immersive platform for art enthusiasts to explore, purchase, and learn about a diverse range of artworks created by talented artists from around the world.",
     },
     {
@@ -20,7 +21,36 @@ const PortfolioPage = () => {
       imageUrl: "/images/portfolio2.png",
       liveDemoUrl: "",
       color: "#e0de57",
+      target: "_self",
       desc: "The art gallery mobile app allows users to browse, order, and purchase artwork conveniently. With a user-friendly interface, users can explore a diverse collection, add artworks to their cart, and securely complete transactions. They can also save favorites, read reviews, and discover artists.",
+    },
+
+    {
+      id: 5,
+      name: "Jobs.af",
+      imageUrl: "/images/jobs.jpeg",
+      liveDemoUrl: "https://jobs.af",
+      color: "#4f8ce2",
+      target: "blank",
+      desc: "     As a member of Netlinks Company's software development team, I had the privilege of collaborating on the creation of Jobs.af, a leading online platform connecting job seekers and employers in Afghanistan. Contributing to this project was an enriching experience, where I played a  role in development and maintenance of the website. My involvement in this megaproject exemplifies my expertise in software development in a big team and my commitment to delivering impactful solutions that drive positive change in the Afghan job market.",
+    },
+    {
+      id: 6,
+      name: "Weena.af",
+      imageUrl: "/images/weena.png",
+      liveDemoUrl: "",
+      color: "#cb2b0f",
+      target: "_self",
+      desc: " As an integral part of the software development team at Netlinks Company, I had the privilege of contributing to the creation of Weena.af, a pioneering website dedicated to facilitating blood donation and distribution across Afghanistan.  Through meticulous teamwork and technical expertise, we developed a user-friendly platform with intuitive interfaces, streamlined donation scheduling, and robust inventory management systems.",
+    },
+    {
+      id: 4,
+      name: "Kamyab Contracting",
+      imageUrl: "/images/portfolio4.png",
+      liveDemoUrl: "https://kamyabcontracting.com",
+      color: "#e2c94f",
+      target: "_blank",
+      desc: "      Kamyab Contracting website, a leader company since 2003, excels in the general contracting field with reshaping residential and commercial spaces with 20+ years of experience",
     },
     {
       id: 3,
@@ -28,15 +58,8 @@ const PortfolioPage = () => {
       imageUrl: "/images/portfolio3.jpeg",
       liveDemoUrl: "",
       color: "#1547a5",
+      target: "_self",
       desc: "Introducing Vidagram: your go-to social media platform for sharing moments, connecting with friends, and discovering new content! Post your favorite videos, photos, and stories, then engage with your community through likes, comments, and private messages. Stay up-to-date with friends' stories and explore trending content—all in one place. Join Vidagram today and start sharing your world!",
-    },
-    {
-      id: 4,
-      name: "Kamyab Contracting",
-      imageUrl: "/images/portfolio4.png",
-      liveDemoUrl: "",
-      color: "#e2c94f",
-      desc: "      Kamyab Contracting website, a leader company since 2003, excels in the general contracting field with reshaping residential and commercial spaces with 20+ years of experience",
     },
   ];
 
